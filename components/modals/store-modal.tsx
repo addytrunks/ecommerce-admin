1e3a10418ba55fce50813131cc9194dfa68dceac
Adhithya Srivatsan <addytrunks@gmail.com>
2023-07-24 14:45:47 +0530

    feat: create store + api

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -3,20 +3,26 @@
 import * as z from 'zod'
 import { useForm } from 'react-hook-form'
 import {zodResolver} from '@hookform/resolvers/zod'
+import axios from 'axios'
 
 import { useStoreModal } from "@/hooks/use-store-modal"
 import Modal from "@/components/ui/modal"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { toast } from 'react-hot-toast'
 
+// Setting up form validations
 const formSchema = z.object({
-    name:z.string().min(5)
+    name:z.string().min(3)
 })
 
 export const StoreModal = () => {
 
     const storeModal = useStoreModal()
+
+    const [loading,setLoading] = useState(false);
 
     // zod : form validation
     const form = useForm<z.infer<typeof formSchema>>({
@@ -27,7 +33,16 @@ export const StoreModal = () => {
     });
 
     const onSubmit = async(values:z.infer<typeof formSchema>) => {
-        console.log(values)
+        try{
+           setLoading(true) 
+           const res = await axios.post('/api/stores',values)
+
+           toast.success('Store successfully created!')
+        } catch (error) {
+            toast.error('Something went wrong 😧')
+        }finally{
+            setLoading(false)
+        }
     }
 
     return (
@@ -40,15 +55,17 @@ export const StoreModal = () => {
                             <FormItem>
                                 <FormLabel>Name</FormLabel>
                                 <FormControl>
-                                    <Input placeholder='E-Commerce' {...field}/>
+                                    {/* ...field => automatically renders props like blur,auto-complete... */}
+                                    <Input disabled={loading} placeholder='E-Commerce' {...field}/>
                                 </FormControl>
+                                {/* Error display message */}
                                 <FormMessage/>
                             </FormItem>
                         )}/>
 
                         <div className='pt-6 space-x-2 flex items-center justify-end w-full'>
-                            <Button variant='outline' onClick={storeModal.onClose}>Cancel</Button>
-                            <Button type='submit'>Continue</Button>
+                            <Button disabled={loading} variant='outline' onClick={storeModal.onClose}>Cancel</Button>
+                            <Button disabled={loading} type='submit'>Continue</Button>
                         </div>
                     </form>
                 </Form>
