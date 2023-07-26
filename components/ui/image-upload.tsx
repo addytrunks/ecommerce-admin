392e416e0ace8ac33df62c84196b8eda3bb00011
Adhithya Srivatsan <addytrunks@gmail.com>
2023-07-26 10:33:02 +0530

    fix : position of delete button

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -34,7 +34,7 @@ const ImageUpload = ({disabled,onChange,onRemove,value}:ImageUploadProps) => {
     <div className="mb-4 flex items-center gap-4">
         {value.map((url) => (
             <div key={url} className="relative w-[200px] h-[200px] rounded-md overflow-hidden">
-                <div className="z-10 top-2 right-2">
+                <div className="z-10 absolute top-2 right-2">
                     <Button type="button" onClick={() => onRemove(url)} variant='destructive' size='icon'>
                         <Trash className="h-4 w-4"/>
                     </Button>
